1dc27cab712f0d4cd0bf5e26ac9959fffb6b39f5
Tom Wilson <tom@jackhq.com>
2013-09-18 22:22:16 -0400

    added animation

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
-angular.module('App', ['firebase'])
+angular.module('App', ['firebase', 'ngRoute', 'ngAnimate','md5', 'ui-gravatar'])
   .config(function($routeProvider, $locationProvider) {
     $routeProvider
-      .when('/', {controller: 'MainCtrl', templateUrl: '/app/templates/main.html'})
-      .when('/posts/new', {controller: 'NewCtrl', templateUrl: '/app/templates/form.html'})
-      .when('/posts/:id/edit', {controller: 'EditCtrl', templateUrl: '/app/templates/form.html'})
-      .when('/posts/:id/comments', {controller: 'CommentsCtrl', templateUrl: '/app/templates/comments.html'})
+      .when('/', {controller: 'MainCtrl', templateUrl: '/app/templates/posts/index.html'})
+      .when('/posts/new', {controller: 'NewCtrl', templateUrl: '/app/templates/posts/form.html'})
+      .when('/posts/:id/edit', {controller: 'EditCtrl', templateUrl: '/app/templates/posts/form.html'})
+      .when('/posts/:id/comments', {controller: 'CommentsCtrl', templateUrl: '/app/templates/comments/index.html'})
       .when('/posts/:id/comments/new', {controller: 'NewCommentCtrl', templateUrl: '/app/templates/comments/form.html'})
       .when('/posts/:post/comments/:id/edit', {controller: 'EditCommentCtrl', templateUrl: '/app/templates/comments/form.html'});
       
--- hackerNews.js
+++ hackerNews.js
@@ -1,10 +1,10 @@
-angular.module('App', ['firebase'])
+angular.module('App', ['firebase', 'ngRoute', 'ngAnimate','md5', 'ui-gravatar'])
   .config(function($routeProvider, $locationProvider) {
     $routeProvider
-      .when('/', {controller: 'MainCtrl', templateUrl: '/app/templates/main.html'})
-      .when('/posts/new', {controller: 'NewCtrl', templateUrl: '/app/templates/form.html'})
-      .when('/posts/:id/edit', {controller: 'EditCtrl', templateUrl: '/app/templates/form.html'})
-      .when('/posts/:id/comments', {controller: 'CommentsCtrl', templateUrl: '/app/templates/comments.html'})
+      .when('/', {controller: 'MainCtrl', templateUrl: '/app/templates/posts/index.html'})
+      .when('/posts/new', {controller: 'NewCtrl', templateUrl: '/app/templates/posts/form.html'})
+      .when('/posts/:id/edit', {controller: 'EditCtrl', templateUrl: '/app/templates/posts/form.html'})
+      .when('/posts/:id/comments', {controller: 'CommentsCtrl', templateUrl: '/app/templates/comments/index.html'})
       .when('/posts/:id/comments/new', {controller: 'NewCommentCtrl', templateUrl: '/app/templates/comments/form.html'})
       .when('/posts/:post/comments/:id/edit', {controller: 'EditCommentCtrl', templateUrl: '/app/templates/comments/form.html'});
       
@@ -25,20 +25,6 @@ angular.module('App', ['firebase'])
     // });
     $rootScope.isAuthor = function(email) {
       return $rootScope.user ? $rootScope.user.email === email : false;
-    };
-  });
-angular.module('App')
-  .controller('CommentsCtrl', function($scope, angularFire, $location, $routeParams) {
-    $scope.postIndex = $routeParams.id;
-    var ref = new Firebase('https://chstechnews.firebaseio.com/posts');
-    angularFire(ref, $scope, 'posts');
-    $scope.$watch('posts', function() {
-      if($scope.posts) { 
-        $scope.post = $scope.posts[$scope.postIndex];
-      }
-    });
-    $scope.back = function() {
-      $location.path('/');
     };
   });
 angular.module('App')
